baede8480c9f3c783c10feb7e97462359dbaabcb
Your Name <you@example.com>
2018-11-18 16:21:27 +0400

    Fixed monthly price calculation

--- StadiumApp/Stadium.xaml.cs
+++ StadiumApp/Stadium.xaml.cs
@@ -36,6 +36,7 @@ namespace StadiumApp
             dtpPlayTime.DisplayDateStart = DateTime.Today;
         }
 
+        //Clear combobox text when click booking button
         private void ClearComponent()
         {
             cmbContacts.Text = "";
@@ -44,32 +45,30 @@ namespace StadiumApp
             dtpPlayTime.SelectedDate = DateTime.Now;
         }
 
+        //Button,combobox and labels are hidden when click booking button
+        private void HideComponents()
+        {
+            cmbStadiums.Visibility = Visibility.Hidden;
+            cmbContacts.Visibility = Visibility.Hidden;
+            btnAddContact.Visibility = Visibility.Hidden;
+            BtnAddStadium.Visibility = Visibility.Hidden;
+            btnBooking.Visibility = Visibility.Hidden;
+            lblContact.Visibility = Visibility.Hidden;
+            lblStadium.Visibility = Visibility.Hidden;
+        }
+
+        #region Fill
+
         //Fill contacts to checkbox to mainWindow from database
         public void fillContacts()
         {
             cmbContacts.Items.Clear();
             foreach (Contacts contacts in db.Contacts)
             {
-                cmbContacts.Items.Add(contacts.Name+" "+contacts.Surname+" "+ contacts.Phone);
+                cmbContacts.Items.Add(contacts.Name + " " + contacts.Surname + " " + contacts.Phone);
             }
         }
 
-      
-
-        //Show AddContactForm 
-        private void btnAddContact_Click(object sender, RoutedEventArgs e)
-        {
-            AddContact addContact = new AddContact(this);
-            addContact.ShowDialog();
-        }
-
-        //Show AddStadiumForm
-        private void BtnAddStadium_Click(object sender, RoutedEventArgs e)
-        {
-            AddStadium addStadium = new AddStadium(this);
-            addStadium.ShowDialog();
-        }
-        
         //Fill Avaible Play Hours to combobox from database
         private void FillHours()
         {
@@ -83,9 +82,17 @@ namespace StadiumApp
             TimeSpan StartTime = new TimeSpan(10, 0, 0);
             TimeSpan EndTime = new TimeSpan(2, 0, 0);
             int interval = (int)(EndTime.Subtract(StartTime).TotalHours + 24);
-            
+
             for (int i = 0; i < interval; i++)
             {
+                Bookings allbooking = new Bookings
+                {
+                    Id = 0,
+                    Time = new TimeSpan(24)
+                };
+
+                cmbSearchHour.Items.Add(allbooking);
+                cmbSearchStadium.SelectedValuePath = "0";
 
                 //int count = db.Bookings.Where(b => b.Date == date.Date && b.Time == StartTime).Count();
                 //if (count == 0)
@@ -103,7 +110,7 @@ namespace StadiumApp
                     StartTime = new TimeSpan(0, 0, 0);
                 }
             }
-    }
+        }
 
         //Fill stadiums to stadium checkbox from database
         public void fillStadiums()
@@ -113,7 +120,7 @@ namespace StadiumApp
             Stadiums stad = db.Stadiums.FirstOrDefault(s => s.Name == cmbStadiums.Text);
             DateTime date = dtpPlayTime.SelectedDate.Value;
 
-            if (cmbHours.SelectedItem!=null)
+            if (cmbHours.SelectedItem != null)
             {
                 string hour = cmbHours.SelectedItem.ToString();
                 TimeSpan time = TimeSpan.Parse(hour);
@@ -123,97 +130,22 @@ namespace StadiumApp
                     cmbStadiums.Items.Add(std.Name);
                 }
             }
-            
+
         }
 
-        //Show available hours when select date from DatePicker
-        private void dtpPlayTime_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-        {
-            cmbHours.Items.Clear();
-            FillHours();
-            
-        }
-
-        //Show available stadiums when select hour from combobox
-        private void cmbHours_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            fillStadiums();
-            
-             if(cmbContacts.SelectedIndex==-1)
-            {
-                fillContacts();
-                cmbStadiums.Visibility = Visibility.Visible;
-                cmbContacts.Visibility = Visibility.Visible;
-                btnAddContact.Visibility = Visibility.Visible;
-                btnBooking.Visibility = Visibility.Visible;
-                BtnAddStadium.Visibility = Visibility.Visible;
-                lblContact.Visibility = Visibility.Visible;
-                lblStadium.Visibility = Visibility.Visible;
-            }
-        }
-        
-        private void btnBooking_Click(object sender, RoutedEventArgs e)
-        {
-
-            TimeSpan time = new TimeSpan(Convert.ToInt32(cmbHours.Text.Split(':')[0]), 0, 0);
-
-            string phone = cmbContacts.Text.Split(' ')[2];
-
-            DateTime date = dtpPlayTime.SelectedDate.Value;
-
-            decimal dec = 30;
-
-            if (time.Hours<starttime.Hours)
-            {
-                date = date.AddDays(1);
-            }
-            
-            Bookings booking = new Bookings
-            {
-                StadiumId = db.Stadiums.FirstOrDefault(s => s.Name == cmbStadiums.Text).Id,
-                Date = date,
-                Time = time,
-                ContactId = db.Contacts.FirstOrDefault(c => c.Phone == phone).Id,
-                Price = dec
-            };
-
-
-            if (cmbContacts.SelectedIndex == booking.ContactId)
-            {
-                MessageBox.Show("Siz artıq bu adla bu eyni vaxt üçün qeydiyyatdan keçmisiniz!");
-            }
-
-            MessageBox.Show("Qeydiyyat tamamlandı!");
-
-            ClearComponent();
-            db.Bookings.Add(booking);
-            db.SaveChanges();
-            //FillHours();
-
-            cmbStadiums.Visibility = Visibility.Hidden;
-            cmbContacts.Visibility = Visibility.Hidden;
-            btnAddContact.Visibility = Visibility.Hidden;
-            BtnAddStadium.Visibility = Visibility.Hidden;
-            btnBooking.Visibility = Visibility.Hidden;
-            lblContact.Visibility = Visibility.Hidden;
-            lblStadium.Visibility = Visibility.Hidden;
-        }
-        
+        //Fill hours to search combobox 
         private void fillSearchHours()
         {
-            //Bookings booking = new Bookings
-            //{
-            //    Id = 0,
-            //    Time =  "Hamısı"
-            //};
-            
-            
+            cmbSearchHour.Items.Add("Hamisi");
+            cmbSearchHour.SelectedIndex = 0;
+
             TimeSpan StartTime = new TimeSpan(10, 0, 0);
             TimeSpan EndTime = new TimeSpan(2, 0, 0);
             int interval = (int)(EndTime.Subtract(StartTime).TotalHours + 24);
 
             for (int i = 0; i < interval; i++)
             {
+                
 
                 cmbSearchHour.Items.Add(StartTime.ToString(@"hh\:mm"));
                 //cmbSearchHour.SelectedValuePath = "0";
@@ -224,8 +156,11 @@ namespace StadiumApp
                     StartTime = new TimeSpan(0, 0, 0);
                 }
             }
+
+           
         }
 
+        //Fill rezerved stadiums to search combobox
         public void fillSearchStadiums()
         {
             cmbStadiums.Items.Clear();
@@ -235,7 +170,7 @@ namespace StadiumApp
                 Id = 0,
                 Name = "Hamisi"
             };
-           
+
             cmbSearchStadium.Items.Add(all);
             cmbSearchStadium.SelectedValuePath = "0";
 
@@ -245,18 +180,81 @@ namespace StadiumApp
             }
 
         }
+        
+        #endregion
 
+        #region Buttons
+
+        //Show AddContactForm 
+        private void btnAddContact_Click(object sender, RoutedEventArgs e)
+        {
+            AddContact addContact = new AddContact(this);
+            addContact.ShowDialog();
+        }
+
+        //Show AddStadiumForm
+        private void BtnAddStadium_Click(object sender, RoutedEventArgs e)
+        {
+            AddStadium addStadium = new AddStadium(this);
+            addStadium.ShowDialog();
+        }
+
+        //Add to database bookings 
+        private void btnBooking_Click(object sender, RoutedEventArgs e)
+        {
+
+            TimeSpan time = new TimeSpan(Convert.ToInt32(cmbHours.Text.Split(':')[0]), 0, 0);
+            string phone = cmbContacts.Text.Split(' ')[2];
+            DateTime date = dtpPlayTime.SelectedDate.Value;
+            decimal dec = 30;
+            
+            if (time.Hours < starttime.Hours)
+            {
+                date = date.AddDays(1);
+            }
+
+
+            Bookings booking = new Bookings
+            {
+                StadiumId = db.Stadiums.FirstOrDefault(s => s.Name == cmbStadiums.Text).Id,
+                Date = date,
+                Time = time,
+                ContactId = db.Contacts.FirstOrDefault(c => c.Phone == phone).Id,
+                Price = dec
+            };
+
+            MessageBox.Show("Qeydiyyat tamamlandı!");
+
+            
+            db.Bookings.Add(booking);
+            db.SaveChanges();
+            //FillHours();
+            ClearComponent();
+            HideComponents();
+        }
+        
+
+        // Finding reserved games by stadium and time
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
             dgBooking.Items.Clear();
 
             int stadiumId = 0;
 
-            if (!string.IsNullOrEmpty(cmbSearchStadium.Text)&&cmbSearchStadium.Text!= "Hamisi")
+            if (!string.IsNullOrEmpty(cmbSearchStadium.Text) && cmbSearchStadium.Text != "Hamisi")
             {
                 stadiumId = db.Stadiums.FirstOrDefault(s => s.Name == cmbSearchStadium.Text).Id;
             }
 
+            TimeSpan alltime = TimeSpan.Parse(cmbSearchHour.Text);
+
+
+            
+            
+
+
+
+            
 
             //string format = @"hh\:mm";
             //Bookings time = db.Bookings.FirstOrDefault();
@@ -273,27 +271,100 @@ namespace StadiumApp
             //// handle validation error
             //}
             //        }
-            //string hour = cmbHours.SelectedItem.ToString();
-            //MessageBox.Show(hour);
-            //TimeSpan time = TimeSpan.Parse(hour);
-
-            List<Bookings> bookings = db.Bookings.Where(b => b.Date == dtpSearchDate.SelectedDate.Value && (stadiumId != 0 ? b.StadiumId == stadiumId : true)/* && (b.Time == time)*/).ToList();
+            string hour = cmbSearchHour.SelectedItem.ToString();
             
-                foreach (Bookings item in bookings.Where(b => b.Date == dtpSearchDate.SelectedDate.Value))
-                {
-                    VwBooking booking = new VwBooking
-                    {
-                        Id = item.Id,
-                        Date = item.Date.Date.ToString("dd.MM.yyyy"),
-                        Hour = item.Time.ToString(@"hh\:mm"),
-                        Stadium = item.Stadiums.Name,
-                        Contact = item.Contacts.Name + " " + item.Contacts.Surname
-                    };
-                    dgBooking.Items.Add(booking);
+            TimeSpan time = TimeSpan.Parse(hour);
 
-                
+            List<Bookings> bookings = db.Bookings.Where(b => b.Date == dtpSearchDate.SelectedDate.Value && (stadiumId != 0 ? b.StadiumId == stadiumId : true) && (b.Time == time)).ToList();
+
+            foreach (Bookings item in bookings.Where(b => b.Date == dtpSearchDate.SelectedDate.Value))
+            {
+                VwBooking booking = new VwBooking
+                {
+                    Id = item.Id,
+                    Date = item.Date.Date.ToString("dd.MM.yyyy"),
+                    Hour = item.Time.ToString(@"hh\:mm"),
+                    Stadium = item.Stadiums.Name,
+                    Contact = item.Contacts.Name + " " + item.Contacts.Surname
+                };
+                dgBooking.Items.Add(booking);
+
+
             }
         }
-        
+
+        #endregion
+
+        #region Selected Change 
+
+        //Show available hours when select date from DatePicker
+        private void dtpPlayTime_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cmbHours.Items.Clear();
+            FillHours();
+
+        }
+
+        //Show available stadiums when select hour from combobox
+        private void cmbHours_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            fillStadiums();
+
+            if (cmbContacts.SelectedIndex == -1)
+            {
+                fillContacts();
+                cmbStadiums.Visibility = Visibility.Visible;
+
+                BtnAddStadium.Visibility = Visibility.Visible;
+
+                lblStadium.Visibility = Visibility.Visible;
+            }
+        }
+
+        //Show available contacts when select stadium from combobox
+        private void cmbStadiums_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cmbContacts.Visibility = Visibility.Visible;
+            lblContact.Visibility = Visibility.Visible;
+            btnAddContact.Visibility = Visibility.Visible;
+        }
+
+        //Show booking button when select stadium from combobox
+        private void cmbContacts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            btnBooking.Visibility = Visibility.Visible;
+        }
+
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Contacts contact = cmbContacts.SelectedValue as Contacts;
+            MessageBox.Show(contact.Id.ToString());
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            int price = 30;
+            DateTime currenDate = dtpSearchDate.SelectedDate.Value;
+            int isPlayed = 0;
+
+            foreach (Bookings item in db.Bookings.ToList())
+            {
+                if (item.Date.Month==currenDate.Month&&item.IsPlayed==true)
+                {
+                    isPlayed++;
+                }
+            }
+
+            lblMonthlyPrice.Content =  (price* isPlayed + " Azn").ToString();
+
+
+              
+            
+        }
     }
 }
